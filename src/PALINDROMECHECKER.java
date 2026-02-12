0a9693dc8e8bb1b90a0cc1d661637376b9ac7e85
Shikhamohapatra <shikhamohapatra204@gmail.com>
2026-02-12 10:42:59 +0530

    UC1 Application Entry & Welcome Message

---
+++ src/PALINDROMECHECKER.java
@@ -0,0 +1,9 @@
+public class PALINDROMECHECKER {
+    public static void main(String[] args){
+        System.out.println("Welcome to palindrome checker app");
+        System.out.println("version 1.0");
+        System.out.println("System initialised successfully");
+   /*USE CASE1: WELCOME CODE FOR PALINDROME CHECKING APP*/
+    }
+
+}
